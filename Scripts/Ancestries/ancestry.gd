f7110904751d753a2a0032157e2b5d2328f1a713
moira-b <moiraysabelle7@gmail.com>
2026-02-22 00:54:51 +0800

    Added name and desc vars for ancestry

--- Scripts/Ancestries/ancestry.gd
+++ Scripts/Ancestries/ancestry.gd
@@ -3,11 +3,12 @@
 class_name Ancestry
 extends Node
 
+@export var ancestry_name: String
+var description: String
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
