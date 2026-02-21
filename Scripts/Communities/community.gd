bcd44781cf3fcb7e8d7f26f565c2320af3add0fa
moira-b <moiraysabelle7@gmail.com>
2026-02-21 23:37:12 +0800

    Created empty script for base community

---
+++ Scripts/Communities/community.gd
@@ -0,0 +1,13 @@
+# This will be the base community script
+# inherited by all character classes.
+class_name Community
+extends Node
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
