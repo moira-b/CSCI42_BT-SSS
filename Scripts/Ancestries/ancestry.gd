bb37a892eb6a3fd155ae8b6b6806b25913094961
moira-b <moiraysabelle7@gmail.com>
2026-02-21 23:38:38 +0800

    Created empty script for base ancestry

---
+++ Scripts/Ancestries/ancestry.gd
@@ -0,0 +1,14 @@
+# This will be the base ancestry class script
+# inherited by all ancestries.
+class_name Ancestry
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
