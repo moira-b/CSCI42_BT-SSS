431ce162222983338a9166952e4cdc4986846e36
moira-b <moiraysabelle7@gmail.com>
2026-02-23 12:17:56 +0800

    Created AncestryFeatures base resource

---
+++ Scripts/Ancestries/AncestryFeatures/ancestry_feature.gd
@@ -0,0 +1,7 @@
+# This will be the base ancestry feature class script
+# inherited by all ancestry features.
+class_name AncestryFeature
+extends Resource
+
+@export var feature_name: String
+@export var feature_description: String
--- Scripts/Ancestries/ancestry.gd
+++ Scripts/Ancestries/ancestry.gd
@@ -1,15 +1,10 @@
 # This will be the base ancestry class script
 # inherited by all ancestries.
 class_name Ancestry
-extends Node
+extends Resource
 
 @export var ancestry_name: String
-var description: String
+@export var description: String
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+@export var feature1: AncestryFeature
+@export var feature2: AncestryFeature
