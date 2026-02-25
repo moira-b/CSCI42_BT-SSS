3edb93cc1868ed852fc5160372b371d5bdc912db
Joaquin Salazar <alonzo.salazar@student.ateneo.edu>
2026-02-26 00:19:07 +0800

    Created Domain Resources

--- Scripts/CharacterClasses/character_class.gd
+++ Scripts/CharacterClasses/character_class.gd
@@ -1,8 +1,14 @@
 # This will be the base character class script
 # inherited by all character classes.
 class_name CharacterClass
-extends Node
+extends Resource
 
+
+@export var name: String
+# TODO: Make domains
+@export var starting_evasion: int
+# TODO: Make hope feature
+@export var class_items: Array[String]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
---
+++ Scripts/Domains/domain.gd
@@ -0,0 +1,5 @@
+# This will be the base class for Domains to be created as resources
+class_name Domain
+extends Resource
+
+@export var name: String
