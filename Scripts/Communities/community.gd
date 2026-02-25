689471be9617a1f0ba049020e3ebec0505dd19df
diego-nst <diegostanalgo1@gmail.com>
2026-02-25 23:49:57 +0800

    Added functions to set community description, feature name, and feature description

--- Scripts/Communities/community.gd
+++ Scripts/Communities/community.gd
@@ -3,6 +3,12 @@
 class_name Community
 extends Resource
 
+@export_enum("Highborne", "Loreborne", "Orderborne", "Ridgeborne", "Seaborne", "Slyborne",
+		"Underborne","Wanderborne","Wildborne") var community_name: String
+var description: String
+var feature_name: String
+var feature_description: String
+
 var all_descriptions: Dictionary = {
 	"Highborne":"Being part of a highborne community means you’re accustomed to a life of elegance, opulence, and prestige within the upper echelons of society. Traditionally, members of a highborne community possess incredible material wealth. While this can take a variety of forms depending on the community—including gold and other minerals, land, or controlling the means of production—this status always comes with power and influence. Highborne place great value on titles and possessions, and there is little social mobility within their ranks. Members of a highborne community often control the political and economic status of the areas in which they live due to their ability to influence people and the economy with their substantial wealth. The health and safety of less affluent people who live in those locations often hinges on the ability of this highborne ruling class to prioritize the well-being of their subjects over profit.
 	\n\nHighborne are often amiable, candid, conniving, enterprising, ostentatious, and unflappable.", 
@@ -49,10 +55,16 @@ var all_features_descriptions = {
 }
 
 var options: Array = ["Highborne", "Loreborne", "Orderborne", "Ridgeborne", "Seaborne", "Slyborne","Underborne","Wanderborne","Wildborne"] 
-@export_enum("Highborne", "Loreborne", "Orderborne", "Ridgeborne", "Seaborne", "Slyborne",
-		"Underborne","Wanderborne","Wildborne") var community_name: String
-# Called when the node enters the scene tree for the first time.
 
+# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var a = ""
+
+func set_description() -> String:	
+	return all_descriptions[community_name]
 	
+func set_feature() -> String:
+	return all_features[community_name]
+	
+func set_feature_description() -> String:
+	return all_features_descriptions[community_name]
