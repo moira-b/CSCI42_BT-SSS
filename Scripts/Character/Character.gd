c2d49de3dd511fd69aba65835241e738958962fa
Leo Sibayan <rafaellosibayan36@gmail.com>
2026-02-26 20:28:02 +0800

    Defined base Character attributes

--- Scripts/Character/Character.gd
+++ Scripts/Character/Character.gd
@@ -1,5 +1,23 @@
 extends Node
 
+@export var characterName: String
+@export var pronouns: Array[String]
+@export var level: int
+@export var agility: int
+@export var strength: int
+@export var finesse: int
+@export var instinct: int
+@export var presence: int
+@export var knowledge: int
+@export var max_hp: int
+@export var current_hp: int
+@export var max_stress: int
+@export var current_stress: int
+@export var max_hope: int
+@export var current_hope: int
+@export var items: Array[String]
+@export var max_armor_slots: int
+@export var current_armor_slots: int
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
