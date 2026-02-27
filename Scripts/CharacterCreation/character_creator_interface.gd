2ab20661dfb40a6f006b8c70ace6b05a9c4a244c
Joaquin Salazar <alonzo.salazar@student.ateneo.edu>
2026-02-27 09:51:45 +0800

    Charactersheet connected

--- Scripts/CharacterCreation/character_creator_interface.gd
+++ Scripts/CharacterCreation/character_creator_interface.gd
@@ -83,3 +83,7 @@ func _on_confirm_button_pressed() -> void:
 func _on_back_button_pressed() -> void:
 	option_tab_index -= 1
 	_set_active_option_tab(option_tab_index)
+
+
+func _on_complete_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/CharacterDisplay/character_sheet.tscn")
