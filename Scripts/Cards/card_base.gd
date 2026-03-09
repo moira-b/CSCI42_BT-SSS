f34e8f43c5c543b99b52199763a23ad23b052e3a
Joaquin Salazar <alonzo.salazar@student.ateneo.edu>
2026-03-09 23:18:04 +0800

    DomainCard accepts json file inputs

--- Scripts/Cards/card_base.gd
+++ Scripts/Cards/card_base.gd
@@ -4,14 +4,32 @@ extends Control
 @export var card_name: String
 var description: String
 var num_counters: int = 0
+var selected_card: Dictionary
 
-var domain_cards_file = "res://Resources/Cards/domain_cards.json"
+var json_to_parse_file
 var json_as_text: String
 var json_as_dict: Dictionary
 
+# Display details
+@onready var name_label: Label = $Name
+@onready var description_label: Label = $Description
+
+
 func _ready() -> void:
 	if self is DomainCard:
-		domain_cards_file = "res://Resources/Cards/domain_cards.json"
+		json_to_parse_file = "res://Resources/Cards/domain_cards.json"
 	
-	json_as_text = FileAccess.get_file_as_string(domain_cards_file)
+	json_as_text = FileAccess.get_file_as_string(json_to_parse_file)
 	json_as_dict = JSON.parse_string(json_as_text)
+	
+	if json_as_dict:
+		set_details()
+		display_details()
+
+func set_details() -> void:
+	selected_card = json_as_dict.get(card_name)
+	description = selected_card.get("description")
+
+func display_details() -> void:
+	name_label.text = card_name
+	description_label.text = description
--- Scripts/Cards/domain_card_base.gd
+++ Scripts/Cards/domain_card_base.gd
@@ -1,6 +1,27 @@
 class_name DomainCard
 extends Card
 
-var domain: Array[Domain]
 var level: int
+var type: String
+var domain: String
 var recall_cost: int
+
+# Display details
+@onready var level_label: Label = $Level
+@onready var domain_label: Label = $Domain
+@onready var recall_cost_label: Label = $RecallCost
+@onready var type_label: Label = $Type
+
+func set_details() -> void:
+	super.set_details()
+	level = int(selected_card.get("level"))
+	type = selected_card.get("type")
+	domain = selected_card.get("domain")
+	recall_cost = int(selected_card.get("recall_cost"))
+
+func display_details() -> void:
+	super.display_details()
+	level_label.text = str(level)
+	domain_label.text = domain
+	recall_cost_label.text = str(recall_cost)
+	type_label.text = type
