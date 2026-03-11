1715580311a29e70fdb6e2fa4224da1ccd495df3
diego-nst <diegostanalgo1@gmail.com>
2026-03-12 01:06:56 +0800

    added the swap card functionality

--- Scripts/Cards/card_vault.gd
+++ Scripts/Cards/card_vault.gd
@@ -5,11 +5,22 @@ var sheet_scene: PackedScene = load("res://Scenes/CharacterDisplay/character_she
 @onready var vault = $Vault
 @onready var displayed_card = $DisplayedCard
 @onready var active_cards = $ActiveCards
+@onready var swap_button = $SwapButton
 
+var card_selected: bool = false
+var selected: DomainCard
+var selected_active_index : int
+var selected_vaulted_index : int
 func enter():
 	character= $Character
 	display_cards()
-	
+
+func _process(_delta):
+	if(card_selected and displayed_card.get_child(0).visible):
+		swap_button.disabled=false
+	else:
+		swap_button.disabled=true
+
 func display_cards():
 	print("active")
 	for card in character.get_child(0).get_children() :
@@ -35,3 +46,23 @@ func _on_vault_item_selected(index: int) -> void:
 	displayed_card.get_child(0).visible=true
 	displayed_card.get_child(0).change_card(character.get_child(1).get_child(index).card_name)
 	
+
+
+func _on_domain_card_selected(card: DomainCard) -> void:
+	card_selected = true
+	selected = card
+	selected_active_index = selected.get_parent().get_index()
+
+func _on_swap_button_pressed() -> void:
+	swap()
+	
+func swap():
+	var selected_active_name = selected.card_name
+	var selected_vaulted_name = $DisplayedCard/DomainCard.card_name
+	$DisplayedCard/DomainCard.change_card(selected_active_name)
+	character.get_child(1).get_child(selected_vaulted_index).change_card(selected_active_name)
+	
+	active_cards.get_child(selected_active_index).get_child(0).change_card(selected_vaulted_name)
+	character.get_child(0).get_child(selected_active_index).change_card(selected_vaulted_name)
+	
+	vault.set_item_text(selected_vaulted_index, selected_active_name)
--- Scripts/Cards/domain_card_base.gd
+++ Scripts/Cards/domain_card_base.gd
@@ -1,6 +1,8 @@
 class_name DomainCard
 extends Card
 
+signal selected(card: DomainCard)
+
 var level: int
 var type: String
 var domain: String
@@ -30,3 +32,10 @@ func change_card(name: String):
 	card_name = name
 	set_details()
 	display_details()
+	
+
+func _on_gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.is_pressed() and event.button_index == MOUSE_BUTTON_LEFT:
+			selected.emit(self)
+			print("Card Clicked")
