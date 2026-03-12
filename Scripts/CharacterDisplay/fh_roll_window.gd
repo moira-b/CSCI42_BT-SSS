9563ece9a898952b2fce3fa3e085a6001059ec00
moira-b <moiraysabelle7@gmail.com>
2026-03-12 21:40:55 +0800

    Add information to FH roll result

--- Scripts/CharacterDisplay/fh_roll_window.gd
+++ Scripts/CharacterDisplay/fh_roll_window.gd
@@ -5,7 +5,9 @@ const MAXIMUM: int = 12
 
 @onready var fear_display = $HBoxContainer/FearDie/VBoxContainer/ResultLabel
 @onready var hope_display = $HBoxContainer/HopeDie/VBoxContainer/ResultLabel
-@onready var result_display = $ResultDisplay
+@onready var sum_display = $ResultsContainer/Sum
+@onready var interpretation_display = $ResultsContainer/Interpretation
+@onready var consequences_display = $ResultsContainer/Consequences
 var fear_result: int = 0
 var hope_result: int = 0
 
@@ -22,11 +24,15 @@ func interpret_rolls() -> void:
 	print("DEBUG: Hope: " + str(hope_result) + " | Fear: " + str(fear_result))
 	
 	if hope_result > fear_result:
-		result_display.text = "Roll with Hope"
+		interpretation_display.text = "Roll with Hope"
+		consequences_display.text = "You gain 1 Hope."
 	elif hope_result==fear_result:
-		result_display.text = "Critical Success!"
+		interpretation_display.text = "Critical Success!"
+		consequences_display.text = "You gain 1 Hope and lose 1 Stress."
 	else:
-		result_display.text = "Roll with Fear"
+		interpretation_display.text = "Roll with Fear"
+		consequences_display.text = "The GM gain 1 Fear."
+	sum_display.text = "Sum: " + str(hope_result + fear_result)
 		
 func _on_roll_again_button_pressed() -> void:
 	fear_result = randi_range(MINIMUM, MAXIMUM)
