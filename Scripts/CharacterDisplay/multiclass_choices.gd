43a9a7fba497c277a8c565936258b94e942ecff7
Joaquin Salazar <alonzo.salazar@student.ateneo.edu>
2026-04-10 02:04:43 +0800

    Added full display to fill all details

--- Scripts/CharacterDisplay/multiclass_choices.gd
+++ Scripts/CharacterDisplay/multiclass_choices.gd
@@ -8,15 +8,28 @@ extends HBoxContainer
 var selected_class: CharacterClass
 var character: Character
 
+func initialize() -> void:
+	_display_multiclass_choices()
+	class_choice_list.item_selected.connect(_on_multiclass_choice_option_selected)
+
 func _display_multiclass_choices() -> void:
 	for character_class in character_classes:
 		class_choice_list.add_item(character_class.name)
 
 func _display_domain_choices() -> void:
+	class_domain_list.clear()
+	
 	var i = 0
-	for domain in selected_class:
-		class_domain_list.add_item(domain)
+	for domain in selected_class.domains:
+		class_domain_list.add_item(domain.name)
 		if character.character_class.domains.has(domain) || character.multiclass_domains.has(domain):
 			class_domain_list.set_item_disabled(i, true)
 		
 		i += 1
+
+func _on_multiclass_choice_option_selected(index: int) -> void:
+	selected_class = character_classes[index]
+	_display_domain_choices()
+
+func get_character(c: Character) -> void:
+	character = c
