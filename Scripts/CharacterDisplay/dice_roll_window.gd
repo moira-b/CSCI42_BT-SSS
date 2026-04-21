0dcdacb4552111be6444525ad7d330276ab3c248
moira-b <moiraysabelle7@gmail.com>
2026-04-21 22:43:31 +0800

    perf: reduced nodes in dice roller window

--- Scripts/CharacterDisplay/dice_roll_window.gd
+++ Scripts/CharacterDisplay/dice_roll_window.gd
@@ -5,11 +5,22 @@ extends Window
 @onready var roll_button = $RollButtonContainer/RollButton
 @onready var dice = $DiceRollUI/Dice
 
-var dice_array: Array
+var dice_2d_array = [
+	[4, null],
+	[6, null],
+	[8, null],
+	[10, null],
+	[12, null],
+	[20, null],
+	[100, null]
+]
+
+var dice_quantity_fields: Array[SpinBox]
 
 func _ready() -> void:
-	#self.visible = false
-	dice_array = dice.get_children()
+	var dice_types = dice.get_children()
+	for i in range(len(dice_types)):
+		dice_2d_array[i][1] = dice_types[i].get_child(0).get_node("SpinBox")
 	
 func _on_roll_button_pressed() -> void:
 	var num_dice: int = 0
@@ -17,16 +28,28 @@ func _on_roll_button_pressed() -> void:
 	var roll: int
 	var results_array: Array
 	
-	for dice_type in dice_array:
-		for i in range(dice_type.count):
-			roll = randi_range(1, dice_type.num_sides)
+	for die in dice_2d_array:
+		var dice_type = die[0]
+		var quantity_rolled = die[1].value
+		
+		for i in range(quantity_rolled):
+			roll = randi_range(1, dice_type)
 			results_array.append(roll)
 			sum += roll
-		#print("DEBUG: " + str(dice_type.count) + "d" + str(dice_type.num_sides))
-		dice_type.count = 0
-		dice_type.value_label.text = str(0)
+		
+		# reset spinbox back to 0
+		die[1].value = 0
 	
-	#print("DEBUG: DICE ROLL RESULT " + str(sum))
+	#for dice_type in dice_array:
+		#for i in range(dice_type.count):
+			#roll = randi_range(1, dice_type.num_sides)
+			#results_array.append(roll)
+			#sum += roll
+		##print("DEBUG: " + str(dice_type.count) + "d" + str(dice_type.num_sides))
+		#dice_type.count = 0
+		#dice_type.value_label.text = str(0)
+	#
+	##print("DEBUG: DICE ROLL RESULT " + str(sum))
 	print(results_array)
 
 	sum_display.text = str(sum)
